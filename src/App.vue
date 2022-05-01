c36abbf7e09225573d4ddf5c76e2c6b2a504ee14
devendr4 <angel.emo.altuve@gmail.com>
2022-04-30 23:09:53 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavBar />
   <router-view />
 </template>
 
+<script lang="ts">
+import NavBar from "@/components/NavBar.vue"; // @ is an alias to /src
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    NavBar,
+  },
+});
+</script>
 <style lang="scss">
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <ul>
+      <li><a>home</a></li>
+      <li><a>about</a></li>
+      <li><a>contact</a></li>
+    </ul>
+  </nav>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NavBar",
+});
+</script>
+
+<style scoped lang="scss">
+nav {
+  width: 100vw;
+}
+
+ul {
+  display: flex;
+  height: 3rem;
+  align-items: center;
+  background-color: red;
+  justify-content: space-around;
+}
+li {
+  list-style-type: none;
+}
+</style>
